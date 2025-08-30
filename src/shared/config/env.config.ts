9ad5571e59ebbcab12f2fb1dc007ad7f7e261017
codewithashim <codewithashim@gmail.com>
2025-08-31 05:48:10 +0600

    update the env config

--- src/shared/config/env.config.ts
+++ src/shared/config/env.config.ts
@@ -1,4 +1,4 @@
 export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1',
+  API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://influence-hub-swart.vercel.app',
 };
